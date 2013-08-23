1b28d463faf18c2add2a0d3e870e3c859876116a
Ankoor Patel <ankoor.p@gmail.com>
2013-08-23 16:04:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vapp::Application.config.secret_token = '2e86e72e39852e67292170e4b02edc0708789046ba1533766c5a2b456e110136a5fde012d211d6e8b94895515f30ebecff21c8d46f67aed58b4e5729b0730d2d'
