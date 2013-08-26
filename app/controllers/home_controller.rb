22482b7c2fa2dfde76a6357ce6d6cf8282de7c70
Ankoor Patel <ankoor.p@gmail.com>
2013-08-26 21:56:40 +0100

    user events added to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   def index
     @events = Event.order('date').page(params[:page]).per(10)
     @groups = Group.page(params[:page]).per(3)
+
+    if current_user
+      @user_events = current_user.events.order('date').page(params[:page]).per(5)
+    end
+
+
   end
 end
