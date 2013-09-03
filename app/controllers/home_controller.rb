1aa3e7cc0c7e5658d24b691d691a9fe4d11753ef
Ankoor Patel <ankoor.p@gmail.com>
2013-09-03 23:43:41 +0100

    responsive videos on group summary - flex-video class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,8 @@ class HomeController < ApplicationController
   # load_and_authorize_resource
 
   def index
-    @search_events = @q.result(distinct: true)
     # @search_events = @q.result(:distinct => true)
-    @groups = Group.page(params[:groups_page]).per(3)
+    @groups = Group.page(params[:groups_page]).per(2)
     if current_user && current_user.attending_events?
       @events = Event.available_events(current_user).by_date.page(params[:events_page]).per(8)
       @user_events = current_user.events.by_date.page(params[:user_events_page]).per(4)
@@ -14,8 +13,7 @@ class HomeController < ApplicationController
   end
 
   def search
-    index
-    render :index
+    @search_events = @q.result(distinct: true).page(params[:events_page]).per(8)
   end
 
 end
