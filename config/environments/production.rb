aaa2d46aae937cb09baf2a364fc9df346787e905
Ankoor Patel <ankoor.p@gmail.com>
2013-09-04 10:15:12 +0100

    Correct AWS varieable names to make it all work on S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Vapp::Application.configure do
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
     }
     config.fog_directory  = 'vapp'
